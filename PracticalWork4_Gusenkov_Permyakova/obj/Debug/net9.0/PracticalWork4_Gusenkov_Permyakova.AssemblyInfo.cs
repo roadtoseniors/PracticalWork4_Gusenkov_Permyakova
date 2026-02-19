0ed6daaaaf159149eef8a968ef78bec39cf34ffc
kekmaster <kekmaster@DESKTOP-AB4C90S>
2026-02-19 11:19:15 +0300

    fuck

--- PracticalWork4_Gusenkov_Permyakova/obj/Debug/net9.0/PracticalWork4_Gusenkov_Permyakova.AssemblyInfo.cs
+++ PracticalWork4_Gusenkov_Permyakova/obj/Debug/net9.0/PracticalWork4_Gusenkov_Permyakova.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PracticalWork4_Gusenkov_Permyakova")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdddcb4305610db9300f69649d6f5e15fe78a844")]
 [assembly: System.Reflection.AssemblyProductAttribute("PracticalWork4_Gusenkov_Permyakova")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PracticalWork4_Gusenkov_Permyakova")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
